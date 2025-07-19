201c91737d7b50518df26cced2eff802129f7ce1
Shalom Junior <shalomjunior007@gmail.com>
2025-07-19 20:33:14 +0100

    updates

--- src/ai-helper/ai-helper.gateway.ts
+++ src/ai-helper/ai-helper.gateway.ts
@@ -11,7 +11,9 @@ import { Socket, Server } from 'socket.io';
 import axios from 'axios';
 // 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
 
-@WebSocketGateway()
+@WebSocketGateway({
+  cors: { origin: '*' },
+})
 export class AiHelperGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   server: Server;
@@ -23,11 +25,21 @@ export class AiHelperGateway implements OnGatewayConnection, OnGatewayDisconnect
   handleDisconnect(client: any) {
     console.log(`Client disconnected: ${client.id}`);
   };
+  
+  @SubscribeMessage('typing')
+  handleTyping(@MessageBody() data: { userId: string }) {
+    this.server.emit('typing', { userId: data.userId });
+  }
 
   @SubscribeMessage('message')
   async handleMessage(@ConnectedSocket() client: any, @MessageBody() payload: any) {
     this.server.emit('message', payload); // Broadcast original message
     try {
+      this.server.emit('typing', {
+        userId: 'Amaka',
+        typing: true,
+      });
+
       const aiResponse = await axios.post(
         'https://openrouter.ai/api/v1/chat/completions',
         {
@@ -41,7 +53,7 @@ export class AiHelperGateway implements OnGatewayConnection, OnGatewayDisconnect
         },
         {
           headers: {
-            Authorization: 'Bearer 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',
+            Authorization: 'Bearer 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',
             'Content-Type': 'application/json',
           },
         },
@@ -49,12 +61,21 @@ export class AiHelperGateway implements OnGatewayConnection, OnGatewayDisconnect
 
       const content = aiResponse.data.choices[0].message.content;
 
+      this.server.emit('typing', {
+        userId: 'Amaka',
+        typing: false,
+      });
+
       this.server.emit('message', {
         userId: 'Amaka',
         message: content,
       });
     } catch (error) {
       console.error('AI Error:', error.response?.data || error.message);
+      this.server.emit('typing', {
+        userId: 'Amaka',
+        typing: false,
+      });
       this.server.emit('message', {
         userId: 'Amaka',
         message: 'Sorry, something went wrong with AI.',
