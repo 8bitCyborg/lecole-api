675998904d2e3bc9ba6eb8c9919a92bddb18e42e
Aeon <shalomsheks@gmail.com>
2025-11-01 12:27:30 +0000

    remove delaymiddleware

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,16 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 
 // delay.middleware.ts
-import { Injectable, NestMiddleware } from '@nestjs/common';
-import { Request, Response, NextFunction } from 'express';
+// import { Injectable, NestMiddleware } from '@nestjs/common';
+// import { Request, Response, NextFunction } from 'express';
 
-@Injectable()
-export class DelayMiddleware implements NestMiddleware {
-  async use(req: Request, res: Response, next: NextFunction) {
-    await new Promise((resolve) => setTimeout(resolve, 2000)); // 1000ms = 1 second
-    next();
-  }
-}
+// @Injectable()
+// export class DelayMiddleware implements NestMiddleware {
+//   async use(req: Request, res: Response, next: NextFunction) {
+//     await new Promise((resolve) => setTimeout(resolve, 2000)); // 1000ms = 1 second
+//     next();
+//   }
+// }
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
