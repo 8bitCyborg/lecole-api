5c003ff77f0a90964cad08a5bf5b92638f20a7f9
Shalom Junior <shalomjunior007@gmail.com>
2026-03-23 18:34:58 +0100

    updates

---
+++ prisma/migrations/20260322153452_change_description_to_motto/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `description` on the `School` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "School" DROP COLUMN "description",
+ADD COLUMN     "motto" TEXT;
