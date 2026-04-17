ec964556df550462af84d01b0e8f43207d0bdb45
Shalom Junior <shalomjunior007@gmail.com>
2026-04-17 16:17:26 +0100

    updates to sameSite policy;

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -85,13 +85,13 @@ export class AuthController {
     res.cookie('access_token', tokens.access_token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: process.env.NODE_ENV === 'production' ? 'strict' : 'none',
       maxAge: 15 * 60 * 1000, // 15 minutes
     });
     res.cookie('refresh_token', tokens.refresh_token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: process.env.NODE_ENV === 'production' ? 'strict' : 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
   }
